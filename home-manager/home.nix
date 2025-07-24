bb7b5ea27216e2c61b5be2517e0fb05e6d440c7b
Albert O'Shea <albertoshea2@gmail.com>
2025-07-24 19:27:13 +1000

    auto: checkpoint before build 2025-07-24T19:27:13+10:00

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -65,12 +65,19 @@
         "privacy.trackingprotection.socialtracking.enabled" = true;
       };
       ExtensionSettings = {
+        # DuckDuckGo Privacy Essentials:
         "jid1-ZAdIEUB7XOzOJw@jetpack" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/duckduckgo-for-firefox/latest.xpi";
           installation_mode = "force_installed";
         };
+        # uBlock Origin:
         "uBlock0@raymondhill.net" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        # 1Password:
+        "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
           installation_mode = "force_installed";
         };
       };
