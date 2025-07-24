d64c604a5a391ed97b14397404c78171859377b7
Albert O'Shea <albertoshea2@gmail.com>
2025-07-24 19:00:57 +1000

    auto: checkpoint before build 2025-07-24T19:00:57+10:00

--- flake.nix
+++ flake.nix
@@ -67,7 +67,7 @@
       "albert@gtx1080shitbox" = home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs {
           system = "x86_64-linux";
-          overlays = builtins.attrValues (import ./overlays {inherit inputs;}) ++ [inputs.nur.overlay];
+          overlays = builtins.attrValues (import ./overlays {inherit inputs;}) ++ [inputs.nur.overlays.default];
           config.allowUnfree = true;
         };
         extraSpecialArgs = {inherit inputs outputs;};
