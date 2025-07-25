5647b323e1a3606e1521ab700b2b44cd0cbf73b0
Albert O'Shea <albertoshea2@gmail.com>
2025-07-25 19:37:24 +1000

    auto: checkpoint before build 2025-07-25T19:37:24+10:00

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -39,14 +39,7 @@
         Locked = true;
       };
       ExtensionUpdate = false;
-      # FIXME(Krey): Review `~/.mozilla/firefox/Default/extensions.json` and uninstall all unwanted
-      # Suggested by t0b0 thank you <3 https://gitlab.com/engmark/root/-/blob/60468eb82572d9a663b58498ce08fafbe545b808/configuration.nix#L293-310
-      # NOTE(Krey): Check if the addon is packaged on https://gitlab.com/rycee/nur-expressions/-/blob/master/pkgs/firefox-addons/addons.json
       ExtensionSettings = {
-        "*" = {
-          installation_mode = "blocked";
-          blocked_install_message = "FUCKING FORGET IT!";
-        };
         "addon@darkreader.org" = {
         	# Dark Reader
         	install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
@@ -72,6 +65,27 @@
     profiles.albert = {
       id = 0;
       isDefault = true;
+      extensions.packages = {
+        "addon@darkreader.org" = {
+        	# Dark Reader
+        	install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+        	installation_mode = "force_installed";
+        };
+        "sponsorBlocker@ajay.app" = {
+        	# Sponsor Block
+        	install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorBlocker@ajay.app.xpi";
+        	installation_mode = "force_installed";
+        };
+        "uBlock0@raymondhill.net" = {
+          # uBlock Origin
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        # 1Password:
+        "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+      }
       /*
       search = {
         force = true;
