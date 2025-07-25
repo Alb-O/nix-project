7f18984e13119b7e01599801d8fc6fb920702e55
Albert O'Shea <albertoshea2@gmail.com>
2025-07-25 23:22:39 +1000

    auto: checkpoint before build 2025-07-25T23:22:39+10:00

--- home-manager/modules/desktop/default.nix
+++ home-manager/modules/desktop/default.nix
@@ -4,21 +4,27 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Dark theme preferences
+  # Dark theme preferences with UI scaling
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
+      # GTK UI scaling - 1.25 = 125% scale
+      text-scaling-factor = 1.25;
     };
   };
 
-  # GTK theme configuration
+  # GTK theme configuration with larger font size
   gtk = {
     enable = true;
     theme = {
       name = "Adwaita-dark";
       package = pkgs.gnome-themes-extra;
     };
-    font.name = "JetBrains Mono";
-    font.package = pkgs.nerd-fonts.jetbrains-mono;
+    font = {
+      name = "JetBrains Mono";
+      package = pkgs.nerd-fonts.jetbrains-mono;
+      # Increase font size from default (usually 11) to 13 for better readability
+      size = 13;
+    };
   };
 }
