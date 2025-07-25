9d6b0957d84bfce7f411100ac50ef5b1faed202e
Albert O'Shea <albertoshea2@gmail.com>
2025-07-26 07:27:25 +1000

    auto: checkpoint before build 2025-07-26T07:27:25+10:00

--- home-manager/modules/programs/firefox.nix
+++ home-manager/modules/programs/firefox.nix
@@ -176,6 +176,10 @@
         "sidebar.revamp" = true;
         "sidebar.verticalTabs" = true;
         "sidebar.main.tools" = "aichat,bookmarks,history";
+        
+        # Use XDG portal
+        "widget.use-xdg-desktop-portal.file-picker" = true;
+
         # Restore session on startup
         "browser.startup.page" = 3;
         "privacy.clearOnShutdown.history" = false;
