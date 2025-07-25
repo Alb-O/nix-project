0ba9d98edaa0080c6f522e7b93489fcfb3214ea3
Albert O'Shea <albertoshea2@gmail.com>
2025-07-25 19:18:56 +1000

    auto: checkpoint before build 2025-07-25T19:18:56+10:00

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -100,7 +100,7 @@
         # };
         "uBlock0@raymondhill.net" = {
           # uBlock Origin
-          install_url = "file:///${self.inputs.firefox-addons.packages.x86_64-linux.ublock-origin}/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/uBlock0@raymondhill.net.xpi";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
           installation_mode = "force_installed";
         };
       };
