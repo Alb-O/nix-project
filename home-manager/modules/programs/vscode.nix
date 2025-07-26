909f5f957684216e208c449b0292cb27df865341
Albert O'Shea <albertoshea2@gmail.com>
2025-07-26 13:26:25 +1000

    auto: checkpoint before build 2025-07-26T13:26:25+10:00

--- home-manager/modules/programs/vscode.nix
+++ home-manager/modules/programs/vscode.nix
@@ -46,7 +46,7 @@
         "editor.fontFamily" = "'JetBrainsMono Nerd Font', 'monospace'";
         "editor.fontSize" = 15;
         "editor.fontLigatures" = true;
-        "editor.lineHeight" = 1;
+        "editor.lineHeight" = 1.5;
 
         # Terminal font configuration
         "terminal.integrated.fontFamily" = "'JetBrainsMono Nerd Font Mono'";
