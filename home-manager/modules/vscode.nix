ec66c1d4fba95de9fcb7626d405c8d2b5ad1c392
Albert O'Shea <albertoshea2@gmail.com>
2025-07-27 12:05:25 +1000

    auto: checkpoint before build 2025-07-27T12:05:25+10:00

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -34,7 +34,7 @@ in {
       usernamehw.errorlens
 
       # Secrets
-      vscode-marketplace."1password".op-vscode
+      "1password".op-vscode
 
       # AI and code assistance
       anthropic.claude-code
